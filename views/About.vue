c04b4a86027cbfe2ef302e5838e1a329b7cf9198
gminnimk <tvt1245@naver.com>
2025-01-10 11:27:36 +0900

    feat: Home, About 페이지 구성

---
+++ views/About.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="about">
+        <div>
+            <h2>Kim Kyung Min</h2>
+            <h2>Kim Kyung Min</h2>
+            <h2>Kim Kyung Min</h2>
+        </div>
+    </section>
+</template>
